6f8ba7f0c298dd551fab5dbcca69553b31f15afb
ginatheducky <ginatheducky@gmail.com>
2026-04-13 19:25:51 +0200

    update to version 2
     ✅ Add El Camin Tab
     ✅ Utilize all character images on Character View
     ✅  On CharacterView, auto-scroll to bottom after status is shown

--- BBQuotes/BBQuotes/BBQuotesApp.swift
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -15,3 +15,14 @@ struct BBQuotesApp: App {
         }
     }
 }
+
+
+/**
+ Version 2 Feature List:
+ ✅ Add El Camin Tab
+ ✅ Utilize all character images on Character View
+ ✅  On CharacterView, auto-scroll to bottom after status is shown
+ - fetch episode data
+ - Extend String to get rid of long image and color names
+ - Create static constants for show names
+ */
